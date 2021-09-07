2c6ddab0a8d1fc720f6c4aea29b643e98ef0015d
unnawut <unnawut@unnawut.com>
2021-09-08 00:18:37 +0700

    Convert to nextjs

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -27,13 +27,23 @@ class CustomDocument extends Document {
             rel="stylesheet"
             href="https://fonts.googleapis.com/icon?family=Material+Icons"
           />
-          <script async src="https://www.googletagmanager.com/gtag/js?id=G-61STCB7S69"></script>
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}"
+          ></script>
           <script>
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-
-            gtag('config', 'G-61STCB7S69');
+            dangerouslySetInnerHTML=
+            {{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                  page_path: window.location.pathname,
+                });
+              `,
+            }}
           </script>
         </Head>
         <body>
