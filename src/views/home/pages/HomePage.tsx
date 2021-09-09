7e394011a50d84b97ab2352708c185f72a3dc319
unnawut <unnawut@unnawut.com>
2021-09-09 18:40:09 +0700

    Add logo

--- src/views/home/pages/HomePage.tsx
+++ src/views/home/pages/HomePage.tsx
@@ -6,17 +6,26 @@ import { LootGallery } from 'views/loot/LootGallery'
 export const HomePage = observer(() => {
   return (
     <Box width="100%" minHeight="50vh" marginTop="64px">
-      <Typography variant="h1" align="center">Your StarLoots!</Typography>
-      <Typography align="center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</Typography>
+      <Typography variant="h1" align="center">
+        StarLoot!
+      </Typography>
+      <Typography align="center">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+        labore et dolore magna aliqua.
+      </Typography>
       <Typography align="center">Input Loot ID to browse or to claim:</Typography>
 
       <Box margin="0 auto" display="flex" alignItems="center">
         <TextField id="loot-id" label="Loot ID" />
-        <Button variant="contained" color="primary">Claim!</Button>
+        <Button variant="contained" color="primary">
+          Claim!
+        </Button>
       </Box>
-      <Loot/>
+      <Loot />
 
-      <Typography variant="h2" align="center">Recently Claimed</Typography>
+      <Typography variant="h2" align="center">
+        Recently Claimed
+      </Typography>
       <LootGallery />
     </Box>
   )
