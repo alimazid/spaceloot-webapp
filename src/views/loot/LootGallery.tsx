1279e06996d66d1b426e397296e9f6072368ca86
supasonk <supasonk@gmail.com>
2021-09-10 19:32:43 +0700

    add type

--- src/views/loot/LootGallery.tsx
+++ src/views/loot/LootGallery.tsx
@@ -1,8 +1,14 @@
 import { observer } from 'mobx-react-lite'
 import { Box, Typography, List } from '@material-ui/core'
 import { LootBox } from 'views/loot/LootBox'
+import { Loot } from 'interfaces/loot.interface'
 
-export const LootGallery = observer((loots) => {
+type Props = {
+  loot?: Loot
+}
+
+
+export const LootGallery = observer((loots : Props) => {
   return (
     <Box
       display="flex"
@@ -11,7 +17,7 @@ export const LootGallery = observer((loots) => {
       maxWidth="1100px"
     >
       {
-        (loots as any).length > 0 && (loots as any).map((loot, index : number) => {
+        (loots as any).length > 0 && (loots as any).map((loot: Loot, index : number) => {
           return (
             <Box key={index} mt={2}>
               <LootBox loot={loot} />
