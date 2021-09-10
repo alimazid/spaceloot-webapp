dee8fb64fe5e2c05a305067ee9d5a034b1c19da8
kaoths <tanawit139@gmail.com>
2021-09-10 19:22:50 +0700

    hide claim button if not claimable

--- src/interfaces/loot.interface.ts
+++ src/interfaces/loot.interface.ts
@@ -1,15 +1,16 @@
 export interface Loot {
-  id: number,
-  owner: string,
-  weapon: string,
-  secondary_weapon: string,
-  shield: string,
-  propulsion: string,
-  material: string,
-  vessel_type: string,
-  class: string,
-  extra: string,
-  name_prefix: string,
-  name_suffix: string,
+  id: number
+  owner: string
+  is_claimed: boolean
+  weapon: string
+  secondary_weapon: string
+  shield: string
+  propulsion: string
+  material: string
+  vessel_type: string
+  class: string
+  extra: string
+  name_prefix: string
+  name_suffix: string
   suffix: string
 }
