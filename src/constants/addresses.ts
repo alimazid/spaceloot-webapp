0edf980b905a58a6f73fdf6e34defba244c5f859
apemon <apemon642002@hotmail.com>
2021-09-10 16:45:44 +0000

    update mainnet contract address

--- src/constants/addresses.ts
+++ src/constants/addresses.ts
@@ -8,7 +8,7 @@ export const addresses: Record<string, { spaceLoot: string, nft: string }> = {
     nft: 'terra1gec7t6zgx66aglarm8twkhjaz00fjtqrmf7v4k'
   },
   mainnet: {
-    spaceLoot: '',
-    nft: ''
+    spaceLoot: 'terra1ec833xj9f8dm0c04cdka9fkyefzgll7pdzzyxq',
+    nft: 'terra13qrc9j00lk3x0rvpptzdmwtckfj64d5g6xnrv9'
   },
 }
