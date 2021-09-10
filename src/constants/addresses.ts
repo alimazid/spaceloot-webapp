dd6b95cbc4756b7280661b7a3bd8509537810b78
unnawut <unnawut@unnawut.com>
2021-09-10 18:58:50 +0700

    Query & display loot properties

--- src/constants/addresses.ts
+++ src/constants/addresses.ts
@@ -1,11 +1,11 @@
-export const addresses: Record<string, { spaceLoot: string }> = {
+export const addresses: Record<string, { spaceLoot: string, nft: string }> = {
   localterra: {
     spaceLoot: '',
-    nft: '',
+    nft: ''
   },
   testnet: {
     spaceLoot: 'terra1e0587xfqxvruxyyrtevwqj5upufltsr0xp6e7e',
-    nft: 'terra1h0kt7mw7nntug88cxdtk0wl6dd3dsn6sj98jup',
+    nft: 'terra1h0kt7mw7nntug88cxdtk0wl6dd3dsn6sj98jup'
   },
   mainnet: {
     spaceLoot: '',
