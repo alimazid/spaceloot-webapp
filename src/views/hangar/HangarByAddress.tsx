947b69eea056ab12799bd4c7ead096d18ff0a849
Jan Jaworski <jaworek3211@gmail.com>
2021-09-13 21:42:04 +0200

    use Link instead of 'a' tag

--- src/views/hangar/HangarByAddress.tsx
+++ src/views/hangar/HangarByAddress.tsx
@@ -34,16 +34,16 @@ const HangarTitle = observer(({ totalLoots, owner }: TitleProps) => {
   }, [totalLoots, isOwned])
 
   const ownerText = useMemo(() => {
-    return isOwned ? 'your hangar' : `${maskWalletAddress(owner)}'s hanger`
-  }, [isOwned])
+    return isOwned ? 'your hangar' : `${maskWalletAddress(owner)}'s hangar`
+  }, [isOwned, owner])
 
   return (
     <Typography variant="h3" className="nes-text is-primary">
       {titleText}{' '}
       <span>
-        <a href={`/captain/${owner}/hangar`} target="_blank" className="nes-text is-warning">
-          {ownerText}
-        </a>
+        <Link href={`/captain/${owner}/hangar`}>
+          <a target={'_blank'} className="nes-text is-warning">{ownerText}</a>
+        </Link>
       </span>
     </Typography>
   )
@@ -111,6 +111,7 @@ export const HangarByAddress = observer(({ owner }: HangarProps) => {
     <BitStarBgContainer py={3}>
       <Box my={3} display="flex" justifyContent="center" alignItems="center">
         <HangarTitle totalLoots={totalLoots} owner={owner} />
+
       </Box>
       <Box display="flex" justifyContent="center">
         <LootHangar
