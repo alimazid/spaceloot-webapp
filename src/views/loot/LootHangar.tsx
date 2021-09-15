62ae296d68b125b30d4d8f32f830e69be485920b
unnawut <unnawut@unnawut.com>
2021-09-15 18:06:02 +0700

    Fix button state after transfer

--- src/views/loot/LootHangar.tsx
+++ src/views/loot/LootHangar.tsx
@@ -6,19 +6,29 @@ import { Loot } from 'interfaces/loot.interface'
 type Props = {
   loots?: Loot[]
   transferable?: boolean | string
+  transferredLoots: number[]
+  setTransferredLoots: any
 }
 
-export const LootHangar = observer(({ loots, transferable }: Props) => {
-  return (
-    <Box display="flex" flexDirection="column" justifyContent="center" maxWidth="1100px">
-      {(loots as any).length > 0 &&
-        (loots as any).map((loot: Loot, index: number) => {
-          return (
-            <Box key={index} mt={2}>
-              <LootBox loot={loot} transferable={transferable} hideOwner />
-            </Box>
-          )
-        })}
-    </Box>
-  )
-})
+export const LootHangar = observer(
+  ({ loots, transferable, transferredLoots, setTransferredLoots }: Props) => {
+    return (
+      <Box display="flex" flexDirection="column" justifyContent="center" maxWidth="1100px">
+        {(loots as any).length > 0 &&
+          (loots as any).map((loot: Loot, index: number) => {
+            return (
+              <Box key={index} mt={2}>
+                <LootBox
+                  loot={loot}
+                  transferable={transferable}
+                  transferredLoots={transferredLoots}
+                  setTransferredLoots={setTransferredLoots}
+                  hideOwner
+                />
+              </Box>
+            )
+          })}
+      </Box>
+    )
+  }
+)
